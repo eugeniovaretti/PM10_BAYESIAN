04fcdc8b355719c977448f112dc9d23e37293aaa
davide-gurrieri <gurrieri99@gmail.com>
2023-02-11 18:47:14 +0100

    added new sections (likelihood choice and prior's hyperparameters tuning) in main.Rmd

--- utils/calcola_param_prior.R
+++ utils/calcola_param_prior.R
@@ -1,16 +1,15 @@
 calcola_param_prior = function(data)
-  # data è una matrice di timeseries (ogni riga è una timeseries)
+  # data è una matrice di timeseries (ogni colonna è una timeseries)
 {
-  p = nrow(data)
-  data = t(data)
+  p = ncol(data)
   fitted_models_AR1 = vector(mode="list",length = p)
-  coefficienti_AR = matrix(0,nrow = p, ncol = 1)
+  coefficienti_AR = rep(0,p)
   sigma_2_i = rep(0,p)
   
   for(i in 1:p)
   {
     fitted_models_AR1[[i]] = arima(data[,i], order = c(1,0,0), include.mean = FALSE)
-    coefficienti_AR[i,] = fitted_models_AR1[[i]]$coef
+    coefficienti_AR[i] = fitted_models_AR1[[i]]$coef
     sigma_2_i[i] = fitted_models_AR1[[i]]$sigma2
   }
   
@@ -19,7 +18,7 @@ calcola_param_prior = function(data)
   rho_0 = mean(coefficienti_AR)
   sigma_2_avg = mean(sigma_2_i)
   var_rho_h = var(coefficienti_AR)
-  lambda = (sigma_2_avg/var_rho_h)[1,1]
+  lambda = (sigma_2_avg/var_rho_h)
   alpha = (sigma_2_avg)^2 / var(sigma_2_i) + 2
   beta = (alpha-1)*sigma_2_avg
   
@@ -28,6 +27,6 @@ calcola_param_prior = function(data)
                    alpha = alpha,
                    beta = beta)
   
-  return(list(coefficienti = coefficienti_AR[,1],
+  return(list(coefficienti = coefficienti_AR,
               parametri = parametri))
 }
--- utils/plot_coefficienti_AR1.R
+++ utils/plot_coefficienti_AR1.R
@@ -1,11 +1,16 @@
-plot_coefficienti_AR1 = function(coefficienti, label1, label2, xlab = "Stations", ylab = "AR1 coefficients")
+plot_coefficienti_AR1 = function(coefficienti, label1, label2, xlab = "Locations", ylab = "AR(1) coefficients")
 {
   library(viridis)
   osservazioni_livelli1 = factor(label1)
   livelli1 = levels(osservazioni_livelli1)
   colori1 = genera_colori(osservazioni_livelli1)$colori
   colori_livelli1 = genera_colori(osservazioni_livelli1)$colori_livelli
-  plot1 = plot(coefficienti, col=colori1, pch=19, xlab = xlab, ylab = ylab)
+  plot1 = plot(coefficienti, 
+               col=colori1,
+               pch=19,
+               xlab = xlab,
+               ylab = ylab,
+               main = "AR(1) coefficients for each location")
   legend("bottomright", legend = livelli1, fill = colori_livelli1, bty="n", cex=0.8)
   plot1_g <- recordPlot()
   dev.off()
@@ -14,7 +19,12 @@ plot_coefficienti_AR1 = function(coefficienti, label1, label2, xlab = "Stations"
   livelli2 = levels(osservazioni_livelli2)
   colori2 = genera_colori(osservazioni_livelli2, viridis_pal(option = "C")(3))$colori
   colori_livelli2 = genera_colori(osservazioni_livelli2, viridis_pal(option = "C")(3))$colori_livelli
-  plot2 = plot(coefficienti, col=colori2, pch=19, xlab = xlab, ylab = ylab)
+  plot2 = plot(coefficienti, 
+               col=colori2,
+               pch=19,
+               xlab = xlab,
+               ylab = ylab,
+               main = "AR(1) coefficients for each location")
   legend("bottomright", legend = livelli2, fill = colori_livelli2, bty="n", cex=0.8)
   plot2_g <- recordPlot()
   dev.off()
