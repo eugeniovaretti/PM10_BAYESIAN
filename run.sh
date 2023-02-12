c713d5120fd6764a63b9d3057fe66f517973e483
davide-gurrieri <gurrieri99@gmail.com>
2023-02-12 22:04:38 +0100

    added run.sh to run the mcmc algo specifying M and a

---
+++ run.sh
@@ -0,0 +1,19 @@
+
+if [ $# -eq 0 ]; then
+    a=175.44202
+    M=0.567
+else
+  a=$1
+  M=$2
+fi
+
+touch bayesmix/examples/ar1nig_hierarchy/in/dp.asciipb
+printf "fixed_value {\n  totalmass: "$M"\n  a: "$a"\n}" > bayesmix/examples/ar1nig_hierarchy/in/dp.asciipb
+
+touch used_parameters.txt
+printf ""$M"\n"$a"" > used_parameters.txt
+
+echo "run the c++ algorithm"
+echo
+cd bayesmix
+examples/ar1nig_hierarchy/runsingle.sh
